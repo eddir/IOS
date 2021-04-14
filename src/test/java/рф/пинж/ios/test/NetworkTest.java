8649ed708b7dbc0336f35bd25ee3719236d683d2
Eduard <eddirworkmail@gmail.com>
2021-04-14 13:17:30 +0400

    Юнит тесты

---
+++ src/test/java/рф/пинж/ios/test/NetworkTest.java
@@ -0,0 +1,12 @@
+package рф.пинж.ios.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NetworkTest {
+    @Test
+    void testGetter() {
+        assertEquals(6*6, 36); // todo: unit тесты
+    }
+}
