09368ecebc15d9ddaabc5cbe834e6e6fa45dbebe
Mikhail <mixail.kuznetsov2001@yandex.ru>
2021-05-28 14:36:32 +0400

    add: сетеры для интерфейса директорий

--- src/main/java/рф/пинж/ios/model/prototype/directory/IDirectory.java
+++ src/main/java/рф/пинж/ios/model/prototype/directory/IDirectory.java
@@ -15,9 +15,21 @@ public interface IDirectory extends IModel {
     String getTitle();
 
     /**
+     * Устанавливает название директории
+     * @param title - название директории
+     */
+    void setTitle(String title);
+
+    /**
      * Возвращает дату создания директории
      */
     String getCreated_at();
+
+    /**
+     * Устанавливает дату создания
+     * @param created_at - дата создания директории
+     */
+    void setCreated_at(String created_at);
 
     /**
      * Возвращает родительскую директорию
@@ -25,9 +37,21 @@ public interface IDirectory extends IModel {
     Directory getParent_dir();
 
     /**
+     * Устанавливает родительскую директорию
+     * @param parent_dir - родительская директория
+     */
+    void setParent_dir(Directory parent_dir);
+
+    /**
      * Возвращает список дочерних директорий
      */
     List<Directory> getChildren_dir();
+
+    /**
+     * Добавляет дочерние директории
+     * @param children_dir - массив дочерних директорий
+     */
+    void setChildren_dir(List<Directory> children_dir);
 
     // files
 }
